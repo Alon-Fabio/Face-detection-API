ff036989ec5c77cdf979655365e6da3675b4140b
Alon-Fabio <alon.the.fabio@gmail.com>
2020-02-20 21:00:58 +0200

    Updated register an added Bcrypt

--- Server/Server.js
+++ Server/Server.js
@@ -50,47 +50,70 @@ const database ={
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.get('/', (req, res)=>{
-    res.send(database)
+
+
+app.get('/profile/:id', (req, res)=>{
+    const {id} = req.params;
+    sqlDB.select('*').from('users').where({id: id})
+    .then((user)=>{
+        if (user.length){
+            console.log(user[0])
+        } else {(console.error('not found')
+        )}
+    }).catch((err)=>res.status(400).json(err));
 });
 app.post('/signin', (req, res)=>{
     if(req.body.password === database.users[0].password && req.body.email === database.users[0].email){
         res.json("sucsses")
     } else {res.status(400).json("err login in")}
 });
-app.post('/register', (req, res)=>{
-    console.log(req.body);
-    sqlDB.insert({name: req.body.name, email: req.body.email, joined: new Date()}).into('users').returning('*')
-    .then((user)=>res.json(user[0]))
-    .catch(err => res.status(400).json('Ops.. there was an error'));
 
-    sqlDB.insert({hash: req.body.password, email: req.body.email}).into('login')
-    .catch(err => res.status(400).json('Ops.. there was an error'));
+
+app.post('/register', (req, res)=>{
+
+    let hash = bcrypt.hashSync(req.body.password, 10);
+    sqlDB.transaction((trx)=>{
+        trx.insert({hash: hash, email: req.body.email}).into('login')
+        .returning('email')
+        .then((LoginEmail)=>{
+            return trx.insert({name: req.body.name, email: LoginEmail[0], joined: new Date()})
+            .into('users').returning('*')
+            .then((user)=>res.json(user[0]));
+        })
+        .then(trx.commit)
+        .then(trx.rollback);
+    }).catch(err => res.status(400).json(err));
+    // sqlDB.insert({hash: hash, email: req.body.email}).into('login')
+    // .returning('email')
+    // .then((LoginEmail)=>{
+    //     sqlDB.insert({name: req.body.name, email: LoginEmail[0], joined: new Date()})
+    //     .into('users').returning('*')
+    //     .then((user)=>res.json(user[0]));
+    // }).catch(err => res.status(400).json(err));
+    
+
+    
 });
 
 app.post('/image',(req, res)=>{
     const id  = req.body.id;
-    let found = false;
-    console.log(req.body.id + " Image req")
-    database.users.forEach(user=>{
-        if(user.id===id){
-            found=true;
-            user.enteries++;
-            console.log(user.enteries)
-            return res.json(user.enteries)
-        }
-    })
-    if (!found) {
-        res.status(400).json('not found');
-    }
-    
+    console.log(id);
+    sqlDB('users')
+    .where('id', '=', id)
+    .increment(
+        'entries', 1
+    )
+    .returning('entries')
+    .then((entries)=>{res.json(entries[0])})
+    .catch((err)=>{res.status(400).json(err)});
+
 })
 
 app.listen(9000, ()=>{
     console.log("Working on port 9000");
 });
 
-setTimeout(()=>{
-    sqlDB.select('*').from('users').then((data)=>{console.log(data)});
-    sqlDB.select('*').from('login').then((data)=>{console.log(data)});
-}, [2000]);
+// setTimeout(()=>{
+//     sqlDB.select('*').from('users').then((data)=>{console.log(data)});
+//     sqlDB.select('*').from('login').then((data)=>{console.log(data)});
+// }, [2000]);
